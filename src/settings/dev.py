8469780b0cb9a93067c679181df003eda50fe62a
SzymonDawi <szymdawi@gmail.com>
2023-12-05 19:15:03 +1300

    Add initial wagtail files (#2)
    * Add initial wagtail files

---
+++ src/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-$$c^j-4rj_p_5qq&+m*k%)2ih4ww$$w+nsow-0tu7*!n9$%gm)"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass

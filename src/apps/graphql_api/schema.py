e43237704d88d7814b8ea7578ecde70d4f2b84d5
SzymonDawi <szymdawi@gmail.com>
2023-12-12 18:55:53 +1300

    Update homepage schema (#13)
    Co-authored-by: Szymon <szymondawi@gmail.com>

--- src/apps/graphql_api/schema.py
+++ src/apps/graphql_api/schema.py
@@ -1,13 +1,47 @@
 import ariadne
+import graphql
+
+from src.apps.home.models import HomePage
 
 query = ariadne.QueryType()
 
+home = ariadne.ObjectType("Home")
+external_link = ariadne.ObjectType("ExternalLink")
+
 
 @query.field("home")
-def resolve_home(*_):
-    return "Hello world!"
+def resolve_home(source, info: graphql.GraphQLResolveInfo, *_):
+    return HomePage.get_page_preview_from_request_or_live(info.context["request"])
+
+
+@home.field("links")
+def resolve_home_links(obj: HomePage, *_):
+    links = []
+
+    for link in obj.homepage_link.all().select_related("link_item"):
+        links.append(link)
+
+    return links
+
+
+@external_link.field("label")
+def resolve_label(obj, *_):
+    return obj.link_item.label
+
+
+@external_link.field("url")
+def resolve_url(obj, *_):
+    return obj.link_item.url
+
+
+@external_link.field("icon_slug")
+def resolve_icon_slug(obj, *_):
+    return obj.link_item.icon_slug
 
 
 schema = ariadne.make_executable_schema(
-    ariadne.load_schema_from_path("schema.graphql"), query
+    ariadne.load_schema_from_path("schema.graphql"),
+    query,
+    home,
+    external_link,
 )

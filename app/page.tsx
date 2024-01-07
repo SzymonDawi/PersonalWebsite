f7a6afa8a3a0c925221a09b483b4857c1d3ea392
SzymonDawi <szymdawi@gmail.com>
2024-01-07 22:18:50 +1300

    Make home page responsive (#30)

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,33 @@
 
 import Link from 'next/link'
 
-import {Col, Row} from 'antd';
+import {Col, Row, Grid} from 'antd';
 
 import Pillar from "./components/pillar";
 import Greeting from "./components/greeting";
 import home_page_styles from "./styles/home_page.module.css";
 
+const { useBreakpoint } = Grid;
+
 export default function Home() {
+  const { sm } = useBreakpoint();
+
+  const nav_col_width = sm ? 15: 24;
+  const greeting_col_width = sm ? 9: 24;
+  const button_col_with = sm ? 8: 24;
+  const row_height = sm ? "50vh" : "100vh"
+  const nav_classname = sm ? "fullRow" : "mobileNav";
+  const nav_button_height = sm ? "100vh" : "100px";
+
   return (
     <Row className="main">
-        <Col span={9}> 
+        <Col span={greeting_col_width} style={{height: row_height}}> 
           <Greeting></Greeting>
         </Col>
-        <Col span={15}>  
-          <Row className="fullRow">
-            <Col span={8} className={home_page_styles.projectPillar}>
-              <Link href="/projects">
+        <Col span={nav_col_width} >  
+          <Row align={"middle"} className={nav_classname}>
+            <Col span={button_col_with} className={home_page_styles.projectPillar} style={{height: nav_button_height}} >
+              <Link href="/projects" style={{height: nav_button_height}}>
                 <Pillar 
                     className={home_page_styles.projectPillar}                        
                     title="Projects"
@@ -26,8 +37,8 @@ export default function Home() {
               </Link>
             </Col>
 
-            <Col span={8} className={home_page_styles.artPillar}>
-              <Link href="/art">
+            <Col span={button_col_with} className={home_page_styles.artPillar} style={{height: nav_button_height}} >
+              <Link href="/art" style={{height: nav_button_height}}>
                 <Pillar     
                     className={home_page_styles.artPillar}   
                     title="Art">
@@ -35,7 +46,7 @@ export default function Home() {
               </Link>
             </Col>
 
-            <Col span={8} className={home_page_styles.aboutMePillar}>
+            <Col span={button_col_with} className={home_page_styles.aboutMePillar} style={{height: nav_button_height}}>
               <Pillar                         
                   className={home_page_styles.aboutMePillar}
                   title="WIP">

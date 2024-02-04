24552704134a076d920370c3fae1630515652aba
Szymon <szymdawi@gmail.com>
2024-02-04 21:29:35 +1300

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+import { backend_url } from './constants';
+
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: backend_url },
+                    { key: "Access-Control-Allow-Methods", value: "GET" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig

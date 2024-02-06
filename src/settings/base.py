b6ec4e0c25aae0c2e25fe2b896d692c7a58ebe9a
Szymon <szymdawi@gmail.com>
2024-02-06 21:04:53 +1300

    Update base settings

--- src/settings/base.py
+++ src/settings/base.py
@@ -190,7 +190,7 @@ STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesSto
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
 AWS_S3_FILE_OVERWRITE = False

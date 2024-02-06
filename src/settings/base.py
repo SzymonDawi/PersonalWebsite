8482e5edf0cf0e14e783f6f2e27cd875171f6b86
Szymon <szymdawi@gmail.com>
2024-02-06 20:00:23 +1300

    Update media url

--- src/settings/base.py
+++ src/settings/base.py
@@ -189,8 +189,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = "/media/"
-
 
 
 if not DEBUG:
@@ -204,7 +202,9 @@ if not DEBUG:
 
     MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
+else:
+    MEDIA_URL = "/media/"
+    
 
 # Wagtail settings
 WAGTAIL_SITE_NAME = "personal_website"

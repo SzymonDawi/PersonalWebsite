86d717c248d301fb94dcdac83718cf6b16fea4bb
Szymon <szymdawi@gmail.com>
2024-02-10 00:27:55 +1300

    Update csp headers

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const cspHeader = `
     object-src 'none';
     base-uri 'self';
     form-action 'self';
-    frame-ancestors 'none' https://www.figma.com;
+    frame-ancestors 'self' https://www.figma.com;
     block-all-mixed-content;
     upgrade-insecure-requests;
 `

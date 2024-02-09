fa6e79d3777aedae8c29eadf47c1431449735f59
SzymonDawi <szymdawi@gmail.com>
2024-02-10 01:01:28 +1300

    Update CSP headers (#42)

--- next.config.js
+++ next.config.js
@@ -2,14 +2,16 @@
 
 const cspHeader = `
     default-src 'self';
+    connect-src 'self' https://personalwebsitewagtailprod.onrender.com https://api.github.com;
     script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
-    font-src 'self';
+    style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+    img-src 'self' blob: data: https://wagtail-media-szymon.s3.ap-southeast-2.amazonaws.com;
+    font-src 'self' https://fonts.gstatic.com;
+    frame-src 'self' https://www.figma.com;
     object-src 'none';
     base-uri 'self';
     form-action 'self';
-    frame-ancestors 'https://www.figma.com/';
+    frame-ancestors 'self';
     block-all-mixed-content;
     upgrade-insecure-requests;
 `

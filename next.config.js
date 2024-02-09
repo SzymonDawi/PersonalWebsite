86fa1bc96474ab5b4ce5b6c477dfb8af7bc89974
Szymon <szymdawi@gmail.com>
2024-02-09 23:34:20 +1300

    Fix broken csp headers

--- next.config.js
+++ next.config.js
@@ -1,5 +1,31 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {}
-
-module.exports = nextConfig
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'https://www.figma.com/';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+`
+ 
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ]
+  },
+}

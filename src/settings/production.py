57877613e2d2ff67c9525b806ba61e8f380a07cf
Szymon <szymdawi@gmail.com>
2024-05-16 21:57:18 +1200

    Update settings

--- src/settings/production.py
+++ src/settings/production.py
@@ -6,8 +6,6 @@ AWS_S3_URL_PROTOCOL = 'https'
 AWS_S3_USE_SSL = True
 AWS_S3_VERIFY = True
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 MEDIA_URL = f'{AWS_S3_URL_PROTOCOL}://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 # Tell Django to copy statics to the `staticfiles` directory
@@ -18,6 +16,7 @@ STORAGES = {
     "default": {
         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
     },
+    "staticfiles": 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 }
 
 

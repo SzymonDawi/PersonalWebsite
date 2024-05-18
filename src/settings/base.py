d18a582b44321cb4b6543f4a44792efc10adb6f2
Szymon <szymdawi@gmail.com>
2024-05-18 15:52:15 +1200

    Update settings

--- src/settings/base.py
+++ src/settings/base.py
@@ -186,8 +186,8 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = "/media/"
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# MEDIA_URL = "/media/"
 
 # Wagtail settings
 WAGTAIL_SITE_NAME = "personal_website"

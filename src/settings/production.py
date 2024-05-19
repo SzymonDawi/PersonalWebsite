8bad84de299a3012a6f23ef60e4b08752b9aa706
Szymon <szymdawi@gmail.com>
2024-05-19 19:05:19 +1200

    Update settings

--- src/settings/production.py
+++ src/settings/production.py
@@ -5,6 +5,11 @@ DEBUG = not env("RENDER")
 AWS_S3_URL_PROTOCOL = 'https'
 AWS_S3_USE_SSL = True
 AWS_S3_VERIFY = True
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+AWS_REGION = env("AWS_REGION")
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_REGION}.amazonaws.com"
 
 MEDIA_URL = f'{AWS_S3_URL_PROTOCOL}://{AWS_S3_CUSTOM_DOMAIN}'
 
@@ -17,7 +22,13 @@ STORAGES = {
         "BACKEND": 'storages.backends.s3boto3.S3Boto3Storage',
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"}
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    },
+    "OPTIONS": {
+        "bucket_name": AWS_STORAGE_BUCKET_NAME,
+        "region_name": AWS_REGION,
+        "custom_domain": AWS_S3_CUSTOM_DOMAIN,
+    }
 }
 
 

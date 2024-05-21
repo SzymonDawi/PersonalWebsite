24fc94042f6c5d6fe51eb293472e7d6bf72852f3
Szymon <szymdawi@gmail.com>
2024-05-21 18:27:45 +1200

    Update settings

--- src/settings/base.py
+++ src/settings/base.py
@@ -78,8 +78,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "corsheaders",
-    "storages",
+    "corsheaders"
 ]
 
 MIDDLEWARE = [

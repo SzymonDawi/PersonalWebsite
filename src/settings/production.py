fd199a46d40c4bb84b7836f062ee696450ca41b3
Szymon <szymdawi@gmail.com>
2024-05-21 18:12:44 +1200

    Update settings

--- src/settings/production.py
+++ src/settings/production.py
@@ -8,11 +8,6 @@ AWS_S3_VERIFY = True
 AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
 AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
-AWS_REGION = env("AWS_REGION")
-AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_REGION}.amazonaws.com"
-
-MEDIA_URL = f'{AWS_S3_URL_PROTOCOL}://{AWS_S3_CUSTOM_DOMAIN}'
-MEDIA_HOST=f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 # Tell Django to copy statics to the `staticfiles` directory
 # in your application directory on Render.
@@ -20,17 +15,16 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STORAGES = {
     "default": {
-        "BACKEND": 'storages.backends.s3boto3.S3Boto3Storage',
+        "BACKEND": "src.storages.PublicMediaStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
-    "OPTIONS": {
-        "bucket_name": AWS_STORAGE_BUCKET_NAME,
-        "region_name": AWS_REGION,
-        "custom_domain": AWS_S3_CUSTOM_DOMAIN,
-    }
 }
+
+MEDIA_ROOT = 'media'
+MEDIA_HOST=f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+MEDIA_URL=f'https://{MEDIA_HOST}/'
 
 
 try:

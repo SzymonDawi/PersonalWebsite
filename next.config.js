410c5903a1d6bfe3ae60594e31492eacf73f1b39
Szymon <szymdawi@gmail.com>
2024-05-22 22:31:27 +1200

    Update img src csp header

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const cspHeader = `
     connect-src 'self' https://personalwebsitewagtailprod.onrender.com https://api.github.com;
     script-src 'self' 'unsafe-eval' 'unsafe-inline';
     style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-    img-src 'self' blob: data: https://wagtail-media-szymon.s3.ap-southeast-2.amazonaws.com;
+    img-src 'self' blob: data: https://wagtail-media-szymon.s3.amazonaws.com http://localhost:8000;
     font-src 'self' https://fonts.gstatic.com;
     frame-src 'self' https://www.figma.com;
     object-src 'none';

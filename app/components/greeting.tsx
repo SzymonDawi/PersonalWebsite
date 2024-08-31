c98101978443ec48ea2ef8d0e276d63da1c5a436
Szymon <szymdawi@gmail.com>
2024-08-31 16:55:29 +1200

    Update greeting to use shadcn

--- app/components/greeting.tsx
+++ app/components/greeting.tsx
@@ -1,20 +1,23 @@
 'use client';
 
-import { Col, Row, Card, Avatar, Space} from 'antd';
-import {GithubOutlined, LinkedinOutlined, InstagramOutlined} from '@ant-design/icons';
 import styles from "../styles/greeting.module.css";
 import {  IconType, useHomeQuery } from '../types/generated';
 import Loader from '../components/loader';
 import { Sideboard } from "../constants";
 import GraphqlError from './graphql_error';
+import {
+    Card,
+    CardTitle,
+    CardImage,
+} from "../components/ui/card"
 
 function getLinkIconComponent(iconType: IconType) {
     if (iconType === IconType.Github){
-        return(<GithubOutlined style={{fontSize: "25px"}}/>)   
+        return(<div className={`${styles.icon} ${styles.github}`}></div>)   
     } else if (iconType === IconType.Instagram){
-        return(<InstagramOutlined style={{fontSize: "25px"}}/>)   
+        return(<div className={`${styles.icon} ${styles.instagram}`}></div>)   
     } else if (iconType === IconType.Linkedin){
-        return(<LinkedinOutlined style={{fontSize: "25px"}}/>)   
+        return(<div className={`${styles.icon} ${styles.linkedIn}`}></div>)   
     }
 }
 
@@ -22,39 +25,27 @@ const Greeting = () => {
     const [{ data, fetching, error }] = useHomeQuery();
   
     if (error) return(<GraphqlError/>);
+    if (fetching) return(<Loader/>);
 
     return(
-        <Row>
-            <Col span={24}>
-                <Card 
-                    className = {styles.greeting}
-                    style={{height: "100vh", paddingTop: "20vh"}}
-                    bordered = {false}
-                    hoverable = {false}>
-                    { fetching && <Loader/>}
-                    <Avatar style={{height: "auto", width: "40%"}} src={data?.home.image.rendition.url}></Avatar>
-                    <p className={`${Sideboard.className} title`} style={{marginTop: "-0.5vw"}}>{data?.home.greeting}</p>  
-                    <Row justify="center" style={{marginTop: "-30px"}}>
-                        <Col lg={{span: 18}} md={{span: 12}} xs={{span: 0}}>
-                            <p className="P-Lato" >{data?.home.description}</p>
-                        </Col>
-                    </Row>
-                    <Row justify="center">
-                        <Space>
-                            {data?.home.links.map(link => {
-                                return(
-                                    <a key={link.label} className={styles.greetingLink} 
-                                    href={link.url} 
-                                    target="_blank">
-                                        {getLinkIconComponent(link.icon_slug)}
-                                    </a>
-                                )
-                            })}
-                        </Space>
-                    </Row>
-                </Card>
-            </Col>
-        </Row>
+        <div className={styles.imageContainer}>
+            <Card>
+                <CardImage className={styles.image} src={`http://localhost:8000${data?.home.image.rendition.url}`}/>
+                <CardTitle className={`${Sideboard.className} ${styles.title}`}>{data?.home.greeting}</CardTitle>
+                <div className={styles.description}>{data?.home.description}</div>
+                <div className={styles.iconContainer}>
+                {data?.home.links.map(link => {
+                    return(
+                        <a key={link.label} className={styles.greetingLink} 
+                        href={link.url} 
+                        target="_blank">
+                            {getLinkIconComponent(link.icon_slug)}
+                        </a>
+                    )
+                })}
+                </div>
+            </Card>
+        </div>
     )
 }
 

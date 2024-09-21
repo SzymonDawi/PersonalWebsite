e04e9365534389d7ea1d0b0e705646fd68f05aef
Szymon <szymdawi@gmail.com>
2024-09-21 18:44:23 +1200

    Update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 set -0 errexit
 
-poetry install --no-root
 
+# poetry install --no-root
+
+echo "running migrations"
 python manage.py migrate
 
+echo "Creating admin"
 python manage.py createsuperuser --noinput
 
 python manage.py collectstatic

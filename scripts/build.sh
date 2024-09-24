dc60088b7487a2e08c34cdcc997115e37e515858
SzymonDawi <szymdawi@gmail.com>
2024-09-24 17:36:19 +1200

    Move frontend to vercel (#65)
    * Update frontend to use vercel

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,8 +3,10 @@ set -0 errexit
 
 poetry install --no-root
 
+echo "running migrations"
 python manage.py migrate
 
-python manage.py createsuperuser --noinput
+echo "Creating admin"
+python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_EMAIL
 
-python manage.py collectstatic
+python manage.py collectstatic --noinput

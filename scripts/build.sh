49753bcfb91ab3b1e4a74681040b51ba89cb1376
Szymon <szymdawi@gmail.com>
2024-09-24 17:22:02 +1200

    Revert unused changes

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 set -0 errexit
 
-
-# poetry install --no-root
-
-echo "install deps"
-python3 -m pip install -r requirements.txt
+poetry install --no-root
 
 echo "running migrations"
-python3 manage.py migrate
+python manage.py migrate
 
 echo "Creating admin"
-python3 manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_EMAIL
+python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_EMAIL
 
-python3 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
--- src/settings/base.py
+++ src/settings/base.py
@@ -18,7 +18,7 @@ import environ
 
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = []
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
@@ -117,7 +117,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "src.wsgi.app"
+WSGI_APPLICATION = "src.wsgi.application"
 
 
 # Database

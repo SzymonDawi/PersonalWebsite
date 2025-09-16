c6c6c579ff10e136f4a53e1a194a03128fa4f85a
Szymon <szymdawi@gmail.com>
2025-09-16 21:14:46 +1200

    Add file type select

--- app/tools/convert-image/page.tsx
+++ app/tools/convert-image/page.tsx
@@ -6,6 +6,7 @@ import styles from "../../styles/projects_page.module.css";
 
 export default function ConvertImage() {
     const [files, setFiles] = useState<File[]>([]);
+    const [targetFileType, setTargetFileType] = useState<string>("");
 
     const handleSlectedFiles = (event: React.ChangeEvent<HTMLInputElement>) => {
         const target = event.target;
@@ -15,12 +16,29 @@ export default function ConvertImage() {
         }
     }
 
+    const handleFileTypeSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const value = event.target.value;
+        setTargetFileType(value);
+        console.log(value);
+    }
+
     return (
         <div>
             <Title componentCat="projectComponent" title="Image Converter" backUrl="/" />
             <div className={styles.projectsContainer}>
                 <h1>image converter</h1>
                 <input type="file" id="imageUpload" accept="image/*" multiple onChange={handleSlectedFiles}/>
+                <h3>Convert to</h3>
+                <select 
+                    id="fileType"
+                    name="fileType" 
+                    style={{width: "200px"}}
+                    value={targetFileType}
+                    onChange={handleFileTypeSelect}>
+                    <option value="PNG">PNG</option>
+                    <option value="JPG">JPG</option>
+    
+                </select>
                 <h3>Files</h3>
                 {files.map((file, index) => {
                     return(
